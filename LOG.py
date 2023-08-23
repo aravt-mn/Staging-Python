150dc85f920076d76ef7a6b3399d0ecb91503ee2
Khuslen <b.khuslen.wish@gmail.com>
2023-08-23 15:41:18 +0800

    Add log

--- LOG.py
+++ LOG.py
@@ -24,6 +24,8 @@ def load_packages():
         package_name = package[0]
         # package_path = f'.C:\Users\DELL LATITUDE E7470\Documents\github\Staging-Python\packages\{package_name}.py'
         package_path = r'.\packages\{}.py'.format(package_name)
+        python_datetime = datetime.now()
+        datetimedd =  python_datetime.strftime('%Y-%m-%d %H:%M:%S')
         # Ensure that the package file exists
         if os.path.exists(package_path):
             # Load the package module dynamically
@@ -31,6 +33,23 @@ def load_packages():
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
             print(f'Loaded package: {package_name}')
+            cursor.execute("SELECT BATCH_NAME, PACKAGE_NAME FROM PackageData WHERE BATCH_NAME = 'Python' AND PACKAGE_ACTIVE = 1")
+            package_data = cursor.fetchall()
+            for packaged in package_data:
+                pack = packaged
+                cursor.execute('''
+                            INSERT INTO [dbo].[BatchLog] (BATCH_NAME, LOAD_STATUS, LOAD_START_DATETIME)
+                            VALUES (?, 'Started', ?);
+                            SELECT SCOPE_IDENTITY() AS LastLogId;
+                            ''', pack[0], datetimedd)
+                cursor.execute("SELECT BATCH_LOG_ID FROM BatchLog WHERE BATCH_NAME='Python' AND LOAD_START_DATETIME = ?",datetimedd)
+                log = cursor.fetchall()
+                            
+                cursor.execute('''
+                            INSERT INTO [dbo].[PackageLog] (BATCH_NAME, PACKAGE_NAME, BATCH_LOG_ID, LOAD_START_DATETIME, LOAD_STATUS)
+                            VALUES (?, ?, ?, ?, 'Started');
+                            SELECT SCOPE_IDENTITY() AS LastLogId;
+                            ''', pack[0], pack[1], log, datetimedd)
             
         else:
             print(f'Package file not found for {package_name}')
